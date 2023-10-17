75e80e487f879424898ef37561a4a9b10fb25ecf
Hunter Germundsen <hg185065@ncr.com>
2023-10-16 21:59:52 -0400

    Removed extra lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image"
 import Link from "next/link"
 
 export default function Home() {
   return (
     <div>
       <h2>This is the home page text</h2>
-      <Image
-        src={"https://storage.googleapis.com/valo-buddy-storage/wBul4sJpC0BbV6PZLxwG-0.png"}
-        alt='A thing'
-        width={370}
-        height={208}
-        quality={100}
-        placeholder='blur'
-        blurDataURL="https://storage.googleapis.com/valo-buddy-storage/wBul4sJpC0BbV6PZLxwG-0.png"
-      />
       <Link href="/ascent">Ascent | </Link>
       <Link href="/bind">Bind | </Link>
       <Link href="/breeze">Breeze | </Link>
