3ebe39a2154fa3b005182585eae0246ac992ea48
Nick Chaloult <npchaloult@gmail.com>
2024-03-03 11:38:40 -0500

    Flesh out agent select screen

--- app/routes/$mapName.tsx
+++ app/routes/$mapName.tsx
@@ -3,24 +3,38 @@ import { Link } from "@remix-run/react";
 
 import { capitalizeWord } from "~/utils";
 
+// Agent images: https://www.valorantpicker.com/#/
+import sova from "../images/sova.png";
+
 export const meta: MetaFunction = ({ params }) => {
   const mapName = capitalizeWord(params.mapName!);
   return [{ title: `Valo Buddy - ${mapName}` }];
 };
 
+interface Agent {
+  name: string;
+  image: string;
+}
+
 export default function AgentSelect() {
+  const agents: Agent[] = [
+    { name: "Sova", image: sova },
+    { name: "Sova", image: sova },
+    { name: "Sova", image: sova },
+  ];
   return (
-    <main>
-      <main className="p-24">
-        <ul>
-          <li>
-            <Link to="sova">Sova</Link>
-          </li>
-          <li>
-            <Link to="killjoy">Killjoy</Link>
-          </li>
-        </ul>
-      </main>
+    <main className="h-full flex justify-center items-center">
+      <div className="grid grid-cols-9">
+        {agents.map(({ name, image }) => (
+          <Link
+            key={name}
+            to={name.toLowerCase()}
+            className="p-1 ring-inset ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 transition"
+          >
+            <img src={image} alt={name} />
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
