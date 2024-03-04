00b04ad5a5768539def45b08b7dd496a5402584d
Nick Chaloult <npchaloult@gmail.com>
2024-03-03 22:41:45 -0500

    Add Filters component

---
+++ app/components/filters.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  filterNames: string[];
+}
+export default function Filters({ filterNames }: Props) {
+  return (
+    <div className="h-full flex justify-between">
+      <div className="flex space-x-4">
+        {filterNames.map((filterName) => (
+          <input
+            key={filterName}
+            type="button"
+            name={filterName}
+            value={filterName}
+            className="px-4 py-2 bg-white/10 rounded-full ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 hover:cursor-pointer transition"
+          />
+        ))}
+      </div>
+      <input
+        type="button"
+        name="clear"
+        value="Clear"
+        className="px-4 py-2 bg-white/10 rounded-full ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 hover:cursor-pointer transition"
+      />
+    </div>
+  );
+}
