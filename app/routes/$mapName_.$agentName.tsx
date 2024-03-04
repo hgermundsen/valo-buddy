7ea239f6dd5865b282868a71666380708e8145ef
Nick Chaloult <npchaloult@gmail.com>
2024-03-03 22:23:47 -0500

    Change styling of "vods" button to better match map selection buttons
    Trying two different button styles for Hunter to a) pick one, and b) to
    see how they're both built.

--- app/routes/$mapName_.$agentName.tsx
+++ app/routes/$mapName_.$agentName.tsx
@@ -18,13 +18,13 @@ export default function VodsOrStratsSelect() {
     <main className="h-full flex justify-center items-center space-x-8 text-4xl font-medium font-['Impact'] uppercase">
       <Link
         to="vods"
-        className="relative p-8 bg-red-400 bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all"
+        className="px-8 py-6 ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 hover:bg-white/10 transition"
       >
         Vods
       </Link>
       <Link
         to="strats"
-        className="relative p-8 bg-red-400 bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all"
+        className="relative px-8 py-6 bg-red-400 bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all duration-500ms"
       >
         Strats
       </Link>
