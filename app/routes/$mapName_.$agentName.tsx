b803ae8804a70d5aa255501c8a12971ab6aab6bd
Nick Chaloult <npchaloult@gmail.com>
2024-03-04 15:22:08 -0500

    Make styling for both vods and strats buttons consistent

--- app/routes/$mapName_.$agentName.tsx
+++ app/routes/$mapName_.$agentName.tsx
@@ -24,7 +24,7 @@ export default function VodsOrStratsSelect() {
       </Link>
       <Link
         to="strats"
-        className="relative px-8 py-6 bg-red-400 bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all duration-500ms"
+        className="px-8 py-6 ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 hover:bg-white/10 transition"
       >
         Strats
       </Link>
