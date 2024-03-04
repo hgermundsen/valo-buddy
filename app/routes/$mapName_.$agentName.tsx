356ce3897c864fcb9c1f2a8b74ea8310d8056531
Nick Chaloult <npchaloult@gmail.com>
2024-03-03 22:55:52 -0500

    Add tracking-wide class

--- app/routes/$mapName_.$agentName.tsx
+++ app/routes/$mapName_.$agentName.tsx
@@ -15,7 +15,7 @@ export const meta: MetaFunction = ({ params }) => {
 
 export default function VodsOrStratsSelect() {
   return (
-    <main className="h-full flex justify-center items-center space-x-8 text-4xl font-medium font-['Impact'] uppercase">
+    <main className="h-full flex justify-center items-center space-x-8 text-4xl font-medium font-['Impact'] uppercase tracking-wide">
       <Link
         to="vods"
         className="px-8 py-6 ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 hover:bg-white/10 transition"
