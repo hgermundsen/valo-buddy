9821d151570be0c4843652d719d9cff905c93c8e
Nick Chaloult <npchaloult@gmail.com>
2024-03-03 22:19:54 -0500

    Style vods and strats buttons

--- app/routes/$mapName_.$agentName.tsx
+++ app/routes/$mapName_.$agentName.tsx
@@ -15,17 +15,19 @@ export const meta: MetaFunction = ({ params }) => {
 
 export default function VodsOrStratsSelect() {
   return (
-    <main>
-      <main className="p-24">
-        <ul>
-          <li>
-            <Link to="vods">Vods</Link>
-          </li>
-          <li>
-            <Link to="strats">Strats</Link>
-          </li>
-        </ul>
-      </main>
+    <main className="h-full flex justify-center items-center space-x-8 text-4xl font-medium font-['Impact'] uppercase">
+      <Link
+        to="vods"
+        className="relative p-8 bg-red-400 bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all"
+      >
+        Vods
+      </Link>
+      <Link
+        to="strats"
+        className="relative p-8 bg-red-400 bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all"
+      >
+        Strats
+      </Link>
     </main>
   );
 }
