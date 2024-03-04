51150e0a4887821347dcbdd8a29d0d361cc2d36c
Nick Chaloult <npchaloult@gmail.com>
2024-03-03 22:54:21 -0500

    Scaffold out vods search and filter UI

--- app/routes/$mapName_.$agentName_.vods.tsx
+++ app/routes/$mapName_.$agentName_.vods.tsx
@@ -1,7 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useParams } from "@remix-run/react";
-import Filters from "~/components/filters";
+import { Form, useParams } from "@remix-run/react";
 
+import Filters from "~/components/filters";
 import { capitalizeWord } from "~/utils";
 
 export const meta: MetaFunction = ({ params }) => {
@@ -19,7 +19,31 @@ export default function Vods() {
 
   return (
     <main className="p-24">
-      <Filters filterNames={["Foo", "Bar"]} />
+      <section>
+        <Form className="flex flex-col space-y-4">
+          <Filters
+            filterNames={[
+              "Filter One",
+              "Filter Two",
+              "Filter Three",
+              "Filter Four",
+              "Filter Five",
+              "Filter Six",
+            ]}
+          />
+
+          <div className="flex space-x-4">
+            <input
+              type="text"
+              placeholder="Search for anything..."
+              className="w-full bg-white/10 p-4"
+            />
+            <button className="relative px-6 bg-red-400 font-medium font-['Impact'] uppercase tracking-wider bottom-0 left-0 hover:bottom-1 hover:left-1 hover:shadow-[-4px_4px_0_white] transition-all duration-500ms">
+              Search
+            </button>
+          </div>
+        </Form>
+      </section>
     </main>
   );
 }
