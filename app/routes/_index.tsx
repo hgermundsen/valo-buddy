63aa8d097112155bae264d8c25da5e8b43d49452
Nick Chaloult <npchaloult@gmail.com>
2024-03-13 17:39:38 -0400

    Make map selection grid 4 columns wide instead of 3

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -34,7 +34,7 @@ export default function MapSelect() {
   ];
   return (
     <main className="p-24">
-      <div className="grid gap-6 grid-cols-3 grid-rows-3">
+      <div className="grid gap-6 grid-cols-4">
         {maps.map(({ name, image }, i) => (
           <Link
             key={name}
