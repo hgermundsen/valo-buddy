dd6fa09240c1514f8219533724c2b016d2e77649
Nick Chaloult <npchaloult@gmail.com>
2024-03-26 15:42:34 -0400

    Display different content depending on whether a user is logged in

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,27 @@
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
 
 export default function Header() {
+  const user = useOptionalUser();
+
   return (
     <header className="flex justify-between p-4">
       <h1>ValoBuddy</h1>
       <nav className="flex space-x-4">
-        <Link to="register">Sign Up</Link>
-        <Link to="login">Log In</Link>
+        {user ? (
+          <>
+            <span>Signed in as {user.email}</span>
+            <Form action="logout" method="post">
+              <button type="submit">Log out</button>
+            </Form>
+          </>
+        ) : (
+          <>
+            <Link to="register">Sign Up</Link>
+            <Link to="login">Log In</Link>
+          </>
+        )}
       </nav>
     </header>
   );
