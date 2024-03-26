4fbf7ca4f803e8cd82164d0dafff8769576ccf1d
Nick Chaloult <npchaloult@gmail.com>
2024-03-26 15:37:36 -0400

    Change number of grid columns on maps page depending on screen width

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -34,7 +34,7 @@ export default function MapSelect() {
   ];
   return (
     <main className="p-24">
-      <div className="grid gap-6 grid-cols-4">
+      <div className="grid gap-6 grid-cols-4 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {maps.map(({ name, image }, i) => (
           <Link
             key={name}
@@ -42,7 +42,7 @@ export default function MapSelect() {
             className="group overflow-hidden opacity-0 animate-rise-and-fade-in ring-2 ring-white/40 hover:ring-4 hover:ring-green-200 transition"
             style={{ animationDelay: `${0.05 * i}s` }}
           >
-            <span className="z-10 absolute bottom-4 left-4 text-4xl font-medium font-['Impact'] uppercase bg-gradient-to-b from-neutral-300 to-white text-transparent bg-clip-text group-hover:text-white transition">
+            <span className="z-10 absolute bottom-2 md:bottom-4 left-2 md:left-4 text-2xl md:text-4xl font-medium font-['Impact'] uppercase bg-gradient-to-b from-neutral-300 to-white text-transparent bg-clip-text group-hover:text-white transition">
               {name}
             </span>
             <img
