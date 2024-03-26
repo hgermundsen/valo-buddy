e5d338d42dd992f94a9a66bd5c3bdad98eb8368e
Nick Chaloult <npchaloult@gmail.com>
2024-03-26 14:33:22 -0400

    Rename /join route to /register

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex justify-between p-4">
       <h1>ValoBuddy</h1>
       <nav className="flex space-x-4">
-        <Link to="join">Sign Up</Link>
+        <Link to="register">Sign Up</Link>
         <Link to="login">Log In</Link>
       </nav>
     </header>
