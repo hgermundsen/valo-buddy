e4461668ccad7427e2e14af8a87feed5de8d5ab2
Nick Chaloult <npchaloult@gmail.com>
2024-04-02 01:22:58 -0400

    Make edit button's hover effect animate in from left to right

--- app/routes/collection_.$mapName_.$agentName_.strats_.$stratId.tsx
+++ app/routes/collection_.$mapName_.$agentName_.strats_.$stratId.tsx
@@ -139,7 +139,7 @@ export default function StratDetailsPage() {
           <h1 className="text-6xl font-medium font-['Impact'] uppercase">
             {data.strat.title}
           </h1>
-          <button className="h-min flex space-x-2 px-5 py-3 font-['Space_Mono'] text-white bg-gradient-to-r from-valored-500 to-red-600 from-50% to-50% bg-left-bottom bg-[length:200%_100%] outline-none hover:bg-right-bottom hover:text-neutral-900 focus:bg-valored-400 transition-all duration-300 ease-in-out">
+          <button className="h-min flex space-x-2 px-5 py-3 font-['Space_Mono'] text-white bg-gradient-to-r from-red-600 to-valored-500 from-50% to-50% bg-right-bottom bg-[length:200%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-valored-400 transition-all duration-300 ease-in-out">
             {/* btw this came from https://flowbite.com/icons/ */}
             {/* TODO: Consider moving this into some kind of shared component.
             /components/icons folder, maybe? */}
