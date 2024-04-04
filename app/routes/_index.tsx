5558a31c2dba36759056c7a040de4507a1d06374
Nick Chaloult <npchaloult@gmail.com>
2024-04-04 15:35:07 -0400

    Add first iteration of text decoding effect on the product name

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,75 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { useEffect, useState } from "react";
 
 export const meta: MetaFunction = () => [{ title: "Valo Buddy" }];
 
 export default function MapSelect() {
+  const [productNameText, setProductNameText] = useState("");
+
+  useEffect(() => {
+    const animationTickDuration = 60; // Milliseconds
+    const encodingChars = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+
+    const curProductNameTextAsArray = ["", "", "", "", "", "", "", "", ""];
+    const finalProductNameTextAsArray = [
+      "V",
+      "a",
+      "l",
+      "o",
+      "B",
+      "u",
+      "d",
+      "d",
+      "y",
+    ];
+
+    // Sliding window approach to produce the "decoding" animation. Each time
+    // the window moves forward by one index, each character inside the window
+    // gets a new decode character. Once a character is no longer inside the
+    // window, it becomes its final, "decoded" character.
+    const windowSize = 8;
+    let startIdx = -1 * (windowSize - 1);
+    let endIdx = 0;
+    const textDecodingInterval = setInterval(() => {
+      // Change out every char inside the window.
+      for (let i = startIdx; i <= endIdx; i++) {
+        if (i < 0 || i >= finalProductNameTextAsArray.length) {
+          continue;
+        }
+        const randomEncodingChar =
+          encodingChars[Math.floor(Math.random() * encodingChars.length)];
+        curProductNameTextAsArray[i] = randomEncodingChar;
+      }
+      // For the character that just exited the window, set it to its "final"
+      // character.
+      const idxThatJustExitedWindow = startIdx - 1;
+      if (idxThatJustExitedWindow >= 0) {
+        curProductNameTextAsArray[idxThatJustExitedWindow] =
+          finalProductNameTextAsArray[idxThatJustExitedWindow];
+      }
+      // Update the product name text with the next "frame".
+      setProductNameText(curProductNameTextAsArray.join(""));
+      // Slide the window forward.
+      startIdx++;
+      endIdx++;
+      // If the animation/effect is finished, clear this interval.
+      if (startIdx > curProductNameTextAsArray.length) {
+        clearInterval(textDecodingInterval);
+      }
+    }, animationTickDuration);
+
+    // When this component is unmounted, clear this interval (just in case the
+    // animation/effect isn't finished when the user navigates away from this
+    // page, for instance).
+    return () => clearInterval(textDecodingInterval);
+  }, []);
+
   return (
     <main className="flex flex-col space-y-32 grow justify-center items-center">
       <MarqueeRtol />
       <div className="flex flex-col items-center space-y-4">
-        <h1 className="text-7xl font-['Space_Mono']">ValoBuddy</h1>
+        <h1 className="text-7xl font-['Space_Mono']">{productNameText}</h1>
         <h2 className="text-xl">
           The <em>ultimate</em> notes system for{" "}
           <span className="text-valored-500 font-bold">
