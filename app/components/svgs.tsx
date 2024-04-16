4287bdb7e3849688d8e75ae4bff20590aac7fb11
Nick Chaloult <npchaloult@gmail.com>
2024-04-16 16:52:44 -0400

    Add "plus" SVG icon

--- app/components/svgs.tsx
+++ app/components/svgs.tsx
@@ -87,3 +87,25 @@ export function CancelIcon() {
     </svg>
   );
 }
+
+export function PlusIcon() {
+  return (
+    <svg
+      className="w-5 h-5"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="butt"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5 12h14m-7 7V5"
+      />
+    </svg>
+  );
+}
