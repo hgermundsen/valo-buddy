940c810dad60938f26db7fc1ad5e4af434411f1b
Nick Chaloult <npchaloult@gmail.com>
2024-04-25 17:14:53 -0400

    Remove italics styling on placeholder text for strat title

--- app/routes/collection_.$mapName_.$agentName_.strats_.$stratId_.edit.tsx
+++ app/routes/collection_.$mapName_.$agentName_.strats_.$stratId_.edit.tsx
@@ -142,7 +142,7 @@ export default function EditStratPage() {
                 name="title"
                 aria-label="Strat title"
                 defaultValue={data.strat.title}
-                className="grow text-5xl font-['Druk_Wide_Bold'] uppercase px-2 py-1 text-neutral-200 placeholder:text-neutral-400 placeholder:italic bg-neutral-700 border-b-2 border-neutral-600 outline-none hover:bg-neutral-600 hover:border-neutral-500 focus:bg-neutral-600 focus:border-neutral-500 transition"
+                className="grow text-5xl font-['Druk_Wide_Bold'] uppercase px-2 py-1 text-neutral-200 placeholder:text-neutral-400 bg-neutral-700 border-b-2 border-neutral-600 outline-none hover:bg-neutral-600 hover:border-neutral-500 focus:bg-neutral-600 focus:border-neutral-500 transition"
               />
               <button
                 type="submit"
