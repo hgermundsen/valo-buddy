8fe0a282ed7eaf5ced1b781dc59481f65c26b010
Nick Chaloult <npchaloult@gmail.com>
2024-04-26 14:36:11 -0400

    Add "close" SVG icon

--- app/components/svgs.tsx
+++ app/components/svgs.tsx
@@ -131,3 +131,27 @@ export function TrashIcon() {
     </svg>
   );
 }
+
+export function CloseIcon() {
+  return (
+    <svg
+      // The width and height is slightly larger than other SVGs in this file on
+      // purpose. This one seems to be a bit smaller for some reason.
+      className="w-6 h-6"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="butt"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 18 17.94 6M18 18 6.06 6"
+      />
+    </svg>
+  );
+}
