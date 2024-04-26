0a9ca638f1701e2c811aecf51d572714bad3f994
Nick Chaloult <npchaloult@gmail.com>
2024-04-26 16:47:48 -0400

    Make content in sections closer together, spread sections farther apart

--- app/routes/collection_.$mapName_.$agentName_.strats_.$stratId_.edit.tags.tsx
+++ app/routes/collection_.$mapName_.$agentName_.strats_.$stratId_.edit.tags.tsx
@@ -82,7 +82,7 @@ export default function EditStratTagsModal() {
 
       <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
         <div className="flex items-center justify-center min-h-full">
-          <div className="flex flex-col space-y-2 p-4 w-[480px] lg:w-1/2 bg-neutral-800">
+          <div className="flex flex-col space-y-4 p-4 w-[480px] lg:w-1/2 bg-neutral-800">
             <div className="flex justify-between items-center">
               <h1 className="text-4xl font-['Druk_Wide_Bold']">EDIT TAGS</h1>
               <button
@@ -93,50 +93,61 @@ export default function EditStratTagsModal() {
                 <CloseIcon />
               </button>
             </div>
-            <div className="flex space-x-2">
-              {sortedTagsForCurStrat.map((tag) => (
-                <div
-                  key={tag.id}
-                  className="px-4 py-2 rounded-full bg-neutral-700 text-sm uppercase font-['Space_Mono']"
+
+            <div className="flex flex-col space-y-2">
+              <h2 className="text-lg text-neutral-400 font-['Space_Mono'] scale-y-125 uppercase">
+                ATTACHED
+              </h2>
+              <div className="flex space-x-2 space-y-2 flex-wrap">
+                {sortedTagsForCurStrat.map((tag) => (
+                  <div
+                    key={tag.id}
+                    className="px-4 py-2 rounded-full bg-neutral-700 text-sm uppercase font-['Space_Mono']"
+                  >
+                    {tag.name}
+                  </div>
+                ))}
+              </div>
+              <fetcher.Form
+                method="post"
+                className="flex justify-between space-x-2"
+              >
+                <input
+                  type="text"
+                  name="newTagName"
+                  placeholder="New tag name..."
+                  aria-label="Create a new tag"
+                  // eslint-disable-next-line jsx-a11y/no-autofocus
+                  autoFocus
+                  className="grow p-3 text-neutral-200 placeholder:text-neutral-400 placeholder:italic bg-neutral-700 border-b-2 border-neutral-600 outline-none hover:bg-neutral-600 hover:border-neutral-500 focus:bg-neutral-600 focus:border-neutral-500 transition"
+                />
+                <button
+                  type="submit"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-['Space_Mono'] text-white bg-gradient-to-r from-red-600 to-valored-500 from-50% to-50% bg-right-bottom bg-[length:200%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-valored-400 transition-all duration-300 ease-in-out"
                 >
-                  {tag.name}
-                </div>
-              ))}
+                  <CreateIcon />
+                  <span>CREATE</span>
+                </button>
+              </fetcher.Form>
             </div>
 
             {/* TODO: Revisit this purely for style reasons. */}
             <hr className="border border-neutral-700" />
 
-            <fetcher.Form
-              method="post"
-              className="flex justify-between space-x-2"
-            >
-              <input
-                type="text"
-                name="newTagName"
-                placeholder="New tag name..."
-                aria-label="Create a new tag"
-                // eslint-disable-next-line jsx-a11y/no-autofocus
-                autoFocus
-                className="grow p-3 text-neutral-200 placeholder:text-neutral-400 placeholder:italic bg-neutral-700 border-b-2 border-neutral-600 outline-none hover:bg-neutral-600 hover:border-neutral-500 focus:bg-neutral-600 focus:border-neutral-500 transition"
-              />
-              <button
-                type="submit"
-                className="flex items-center space-x-2 px-4 py-3 text-sm font-['Space_Mono'] text-white bg-gradient-to-r from-red-600 to-valored-500 from-50% to-50% bg-right-bottom bg-[length:200%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-valored-400 transition-all duration-300 ease-in-out"
-              >
-                <CreateIcon />
-                <span>CREATE</span>
-              </button>
-            </fetcher.Form>
-            <div className="flex space-x-2">
-              {sortedTagsForOtherStrats.map((tag) => (
-                <div
-                  key={tag.id}
-                  className="px-4 py-2 rounded-full bg-neutral-700 text-sm uppercase font-['Space_Mono']"
-                >
-                  {tag.name}
-                </div>
-              ))}
+            <div className="flex flex-col space-y-2">
+              <h2 className="text-lg text-neutral-400 font-['Space_Mono'] scale-y-125 uppercase">
+                OTHERS
+              </h2>
+              <div className="flex space-x-2">
+                {sortedTagsForOtherStrats.map((tag) => (
+                  <div
+                    key={tag.id}
+                    className="px-4 py-2 rounded-full bg-neutral-700 text-sm uppercase font-['Space_Mono']"
+                  >
+                    {tag.name}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
