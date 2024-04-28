d7a521fbb5aa6c838885b660fdc7cf4feca8115b
Nick Chaloult <npchaloult@gmail.com>
2024-04-27 23:26:34 -0400

    Add borders to sign up and log in buttons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -193,13 +193,13 @@ export default function MapSelect() {
         <div className="flex space-x-4">
           <Link
             to="register"
-            className="p-5 font-['Space_Mono'] text-white bg-gradient-to-r from-red-600 to-valored-500 from-50% to-50% bg-right-bottom bg-[length:200%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-valored-400 transition-all duration-300 ease-in-out"
+            className="p-5 font-['Space_Mono'] text-white border-2 border-red-300 bg-gradient-to-r from-red-600 to-valored-500 from-50% to-50% bg-right-bottom bg-[length:200%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 hover:border-valored-500 focus:bg-valored-400 transition-all duration-300 ease-in-out"
           >
             SIGN UP NOW
           </Link>
           <Link
             to="login"
-            className="p-5 font-['Space_Mono'] text-white bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 focus:bg-valored-400 transition-all duration-300 ease-in-out"
+            className="p-5 font-['Space_Mono'] text-white border-2 border-neutral-700 bg-gradient-to-r from-red-600 to-neutral-900 from-50% to-50% bg-right-bottom bg-[length:201%_100%] outline-none hover:bg-left-bottom hover:text-neutral-900 hover:border-valored-500 focus:bg-valored-400 transition-all duration-300 ease-in-out"
           >
             LOG IN
           </Link>
