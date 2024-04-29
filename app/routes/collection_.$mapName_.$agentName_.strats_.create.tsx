db09adb1b550e4bb0d112beec7bf9c6c53d4e39e
Nick Chaloult <npchaloult@gmail.com>
2024-04-28 22:10:56 -0400

    Conditionally render things like "delete strat" button if we're creating
    If the user just created a new strat, then they'll be brought to the
    edit strat page. Since it's brand new, we don't need to render things
    like the "delete strat" button on that page. The "cancel" button should
    also behave differently: if we're creating a new strat, it should delete
    that blank strat which was just created, triggering an action, instead
    of just navigating the browser's history back by one.
    
    Perform this conditional rendering based on whether the
    "isNewlyCreatedStrat" key/value pair is present on the session cookie.

--- app/routes/collection_.$mapName_.$agentName_.strats_.create.tsx
+++ app/routes/collection_.$mapName_.$agentName_.strats_.create.tsx
@@ -2,14 +2,25 @@ import { ActionFunctionArgs, redirect } from "@remix-run/node";
 
 import { createEmptyStrat } from "~/models/strat.server";
 import { getMapNameAndAgentName } from "~/security";
-import { requireUserId } from "~/session.server";
+import { requireUserId, sessionStorage } from "~/session.server";
 
 export async function action({ request, params }: ActionFunctionArgs) {
   const userId = await requireUserId(request);
   const { mapName, agentName } = getMapNameAndAgentName(params);
 
+  const session = await sessionStorage.getSession(
+    request.headers.get("Cookie"),
+  );
+
   const newEmptyStrat = await createEmptyStrat(mapName, agentName, userId);
+  // The "edit strats" page looks for this key/value pair on the session cookie.
+  // It renders slightly different content if it's present.
+  session.set("isNewlyCreatedStrat", true);
 
   const newEmptyStratPageURL = `/collection/${mapName}/${agentName}/strats/${newEmptyStrat.id}/edit`;
-  return redirect(newEmptyStratPageURL);
+  return redirect(newEmptyStratPageURL, {
+    headers: {
+      "Set-Cookie": await sessionStorage.commitSession(session),
+    },
+  });
 }
